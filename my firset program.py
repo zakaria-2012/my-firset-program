72840523a5b7bc045669de23d4128ab7d2a643a9
zakaria-2012 <zakariahadri4@gmail.com>
2026-05-22 15:53:31 +0300

    Add files via upload

---
+++ my firset program.py
@@ -0,0 +1,10 @@
+name = 'zakaria'
+age = 14
+height = 1.64
+is_student = True
+favorite_color = 'red'
+hobbies = ['reading', 'coding', 'traveling']
+person = {'name': 'zakaria', 'age': 14, 'height': 1.64, 'is_student': True, 'favorite_color': 'red', 'hobbies': ['reading', 'coding', 'traveling']}
+print(person) 
+
+
